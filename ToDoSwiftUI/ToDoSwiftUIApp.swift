e3510091cb692f0a85f1f1adf3eca0f9460faa70
Avinash Patil <jarvis.avinash@gmail.com>
2021-03-23 14:09:21 +0530

    Initial Commit

---
+++ ToDoSwiftUI/ToDoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftUIApp.swift
+//  ToDoSwiftUI
+//
+//  Created by Avinash on 23/03/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
